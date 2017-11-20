c230730278ce815bb0c6bf674bdbfdee8a92f7d4
corhaneanusergiu <corhaneanusergiu@gmail.com>
2017-11-20 11:34:36 +0200

    variabile

--- Atelier/minimal initial.ino
+++ Atelier/minimal initial.ino
@@ -16,7 +16,7 @@
 // Releu Sirena
 // REED (9 BUC)
 // PIR (2 BUC)
-// Buton ARM / DEZARM
+// Switch ARMAT / DEZARMat
 // RGB LED
 // BUZZER
 // --------------------------
@@ -45,8 +45,8 @@
 #define FUM 3
 #define TEMPERATURA 4
 
-int stare = 0;
-int stare_anterioara = 0;
+int stare_alarma = 0;
+int stare_anterioara_alarma = 0;
 int led = 0;
 int buton = 0;
 int sirena_ext = 0;
@@ -54,6 +54,53 @@ int sirena_int = 0;
 int buzzer = 0;
 int capacitiv = 0;
 int nr_senzori = 0;
+
+int enable_sensor_reactivation=0;
+unsigned long alarm_timeout = 1000; //set waiting time before turning off the siren once the sensor alarm is off
+unsigned long grace_period = 10000; //alarm grace period
+unsigned long lcd_bk_period = 8000; //backlight duration
+unsigned long siren_start_timeout = 5000; //avoid duplicate alarm start/stop request from webserver
+unsigned long alarm_standby_timeout = 300; //time before siren starts again while the alarm signal is alarmed
+
+//------ VARIABILE GENERALE -------
+#define reboot_count 99
+#define adresa_stare_anterioara_alarma 100 // adresa din eeprom a starii anterioare
+
+boolean enable_alarm = false;
+boolean enable_pir = true;
+boolean enable_reed = true;
+boolean alarm_armed = false;
+boolean alarm = false;
+
+unsigned long nfc_ts = millis();
+unsigned long lcd_ts = millis();
+unsigned long lcd_message_ts = millis();
+unsigned long lcd_bk_ts = millis();
+unsigned long siren_start_ts = millis();
+unsigned long reset_sensors_ts = millis();
+unsigned long alarm_standby_timeout_ts = millis();
+unsigned long alarm_delay_ts = millis();
+unsigned long grace_period_ts = millis();
+
+int prev_sec = 0;
+
+static int nfc_period = 1000; //nfc read frequency
+
+
+bool menu_enabled = false;
+int menu_option = 0;
+
+long int alarm_timeout_ts;
+bool alarm_siren_started = false;
+bool alarm_standby = false;
+bool force_alarm = false;
+bool check_sensors_before_activation = false;
+
+char tmp[30];
+int tmp_int;
+unsigned long tmp_ulong;
+unsigned long delay_ts;
+
 
 
 
